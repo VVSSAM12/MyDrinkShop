6848868fc16869cc2fafbf71669d13e2f9a4ce8c
Sebastian Pahomi <sebiflorinp@gmail.com>
2026-05-12 10:56:18 +0300

    More fixes

--- src/main/java/drinkshop/service/ReceiptService.java
+++ src/main/java/drinkshop/service/ReceiptService.java
@@ -3,7 +3,6 @@ package drinkshop.service;
 import drinkshop.domain.Order;
 import drinkshop.domain.OrderItem;
 import drinkshop.domain.Product;
-import drinkshop.repository.Repository;
 
 import java.util.List;
 
@@ -12,7 +11,10 @@ public class ReceiptService {
         StringBuilder sb = new StringBuilder();
         sb.append("===== BON FISCAL =====\n").append("Comanda #").append(o.getId()).append("\n");
         for (OrderItem i : o.getItems()) {
-            Product p = products.stream().filter((p1)->i.getProduct().getId()==p1.getId()).toList().get(0);
+            Product p = products.stream()
+                    .filter(p1 -> i.getProduct().getId() == p1.getId())
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("Product not found for order item"));
             sb.append(p.getNume()+": ").append(p.getPret()).append(" x ").append(i.getQuantity()).append(" = ").append(i.getTotal()).append(" RON\n");
         }
         sb.append("---------------------\nTOTAL: ").append(o.getTotalPrice()).append(" RON\n=====================\n");
