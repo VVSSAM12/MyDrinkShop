8386ace53896be04f9b8b6ae9598921e182b3991
Sebastian Pahomi <sebiflorinp@gmail.com>
2026-05-12 11:01:47 +0300

    Jenkin fixesiiiiiiiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjl

--- src/main/java/drinkshop/service/StocService.java
+++ src/main/java/drinkshop/service/StocService.java
@@ -7,7 +7,7 @@ import drinkshop.repository.Repository;
 import drinkshop.service.validator.StocValidator;
 
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StocService {
 
@@ -67,7 +67,7 @@ public class StocService {
 
             List<Stoc> ingredienteStoc = stocRepo.findAll().stream()
                     .filter(s -> s.getIngredient().equalsIgnoreCase(ingredient))
-                    .toList();
+                    .collect(Collectors.toList());
 
             double ramas = necesar;
 
